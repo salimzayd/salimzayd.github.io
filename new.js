98eb9cc24caf463d0ec3658f39210684022395a6
salimzayd <zidsalim786@gmail.com>
2023-11-08 15:52:30 +0530

    first commit

---
+++ new.js
@@ -0,0 +1,21 @@
+function sendmail(){
+    var params = {
+        name : document.getElementById("name").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
+    };
+    const serviceID = "service_17pvrhe";
+    const templateID = "template_mi321zb";
+    
+    emailjs
+    .send(serviceID,templateID,params)
+    .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("your message send successfully");
+        })
+        .catch((err) => console.log(err));
+}
+
